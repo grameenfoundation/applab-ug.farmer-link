182833d6eacc278011742fef7cd1fd3b07849e7f
James O. Onyango <jonyango@applab.org>
2012-07-03 10:12:02 +0300

    First commit

---
+++ src/applab/client/farmerlink/OptionsActivity.java
@@ -0,0 +1,13 @@
+package applab.client.farmerlink;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OptionsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
